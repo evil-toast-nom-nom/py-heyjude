c9627be5dda517dbddc462f06d3ba32cef220dae
1337 <fvdm@vdm.io>
2024-01-20 15:03:19 +0200

    Fixed baseurl issues.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="py-heyjude",
-    version="0.1.1",
+    version="0.1.2",
     author="Evil Toast",
     description="A Python wrapper for the HeyJude API",
     long_description=long_description,
